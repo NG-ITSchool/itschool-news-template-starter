a4e652eff2770d386f479e31294168d4c9fe9703
Cujba Victor <cujba.victor96@gmail.com>
2023-03-13 20:16:09 +0100

    add-context-favorites

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { Home } from "./pages/Home";
 import { Favorites } from "./pages/Favorites";
 import { NewsCategory } from "./pages/NewsCategory";
 import { NewsDetails } from "./pages/NewsDetails";
+import { FavoritesContext } from "./store/Favorites/context";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/category/:categoryId" element={<NewsCategory />} />
-        <Route path="/news/:newsId/*" element={<NewsDetails />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
+      <FavoritesContext.Provider value={}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/category/:categoryId" element={<NewsCategory />} />
+          <Route path="/news/:newsId/*" element={<NewsDetails />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </FavoritesContext.Provider>
     </div>
   );
 }
